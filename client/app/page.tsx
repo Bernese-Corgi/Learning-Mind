7b8bcd355b1b72d9ac9d521561d25c434b613f98
Bernese-Corgi <jnyng0621@gmail.com>
2023-07-31 22:28:33 +0900

    Feat: Front init next.js

---
+++ client/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1 className="">Learning Tool</h1>
+    </main>
+  )
+}
